86ba28b7ebf6f041b7398173e6ca341accc27671
Boris Zupancic <18bz20@queensu.ca>
2026-05-13 12:22:17 -0400

    Updated NewtonPolyhedron

--- src/ResolutionOfSingularities/Polyhedra.py
+++ src/ResolutionOfSingularities/Polyhedra.py
@@ -10,15 +10,15 @@ from sage.all import QQ, VectorSpace, vector, PolynomialRing, Polyhedron
 from sage.geometry.newton_polygon import NewtonPolygon 
 
 def NewtonPolyhedron(Y: AlgebraicScheme_subscheme_affine, xi: MultivectorField, U: DiffChart):
-    p1 = NewtonPolygon([]) 
+    p1 = Polyhedron([]) 
     if Y is not None:
         vertices = []
         for f in Y.defining_ideal().gens():
             for exp in f.exponents():
                 vertices.append(vector(exp))
-        p1 = NewtonPolygon(vertices)
+        p1 = Polyhedron(vertices = vertices)
 
-    p2 = NewtonPolygon([]) 
+    p2 = Polyhedron([]) 
     if xi is not None:
         e = VectorSpace(QQ,xi.domain().dim()).basis()
         R = PolynomialRing(QQ, U[:])
@@ -28,14 +28,14 @@ def NewtonPolyhedron(Y: AlgebraicScheme_subscheme_affine, xi: MultivectorField, 
             for exp in R(str(coeff)).exponents():
                 v = vector(exp) - w
                 vertices.append(v)
-        p2 = NewtonPolygon(vertices)
+        p2 = Polyhedron(vertices = vertices)
     return p1, p2
 
 def AdmissiblePolyhedron(Y: AlgebraicScheme_subscheme_affine, xi:MultivectorField, U:DiffChart):
     n = Y.ambient_space().ngens()
-    Newt_Y, Newt_xi = NewtonPolyhedron_V2(Y,xi, U)
-    M1 = [[-1] + list(v) for v in Newt_Y.vertices()] #Y-admissibility
-    M2 = [[0] + list(v) for v in Newt_xi.vertices()] #xi-admissibiliy
+    Newt_Y, Newt_xi = NewtonPolyhedron(Y,xi, U)
+    M1 = [[-1] + v for v in Newt_Y.vertices_list()] #Y-admissibility
+    M2 = [[0] + v for v in Newt_xi.vertices_list()] #xi-admissibiliy
     M3 = [[0] + [1 if i==j else 0 for i in range(n)] for j in range(n)] #non-negativity of coordinate weights
     return Polyhedron(ieqs = M1 + M2 + M3)
 
